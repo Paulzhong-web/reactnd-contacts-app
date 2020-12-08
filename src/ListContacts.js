06711513c20dfb3a4c06b51be64e008df779c552
Paulzhong-web <paulzhong114@gmail.com>
2020-12-08 18:59:31 -0500

    clean up code

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,32 +1,5 @@
 import React from 'react';
 
-// class ListContacts extends Component {
-//     render() {
-//         // console.log('Props', this.props)
-//         return (
-//             <ol className='contact-list'>
-//                 {this.props.contacts.map((contact) => (
-//                     <li key={contact.id} className='contact-list-item'>
-//                         <div
-//                             className='contact-avatar'
-//                             style={{
-//                                 backgroundImage: `url(${contact.avatarURL})`
-//                             }}
-//                         ></div>
-//                         <div className='contact-details'>
-//                             <p>{contact.name}</p>
-//                             <p>{contact.handle}</p>
-//                         </div> 
-//                         <button className='contact-remove'>
-//                             Remove
-//                         </button>
-//                     </li>
-//                 ))}
-//             </ol>
-//         )
-//     }
-// }
-
 function ListContacts (props) {
     return (
         <ol className='contact-list'>
