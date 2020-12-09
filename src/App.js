5e8892f3f5c24aac0796ac660330f9a25995f85f
Paulzhong-web <paulzhong114@gmail.com>
2020-12-09 14:59:32 -0500

    Hook up delete button to remove contacts

--- src/App.js
+++ src/App.js
@@ -25,11 +25,29 @@ class App extends Component {
      ]
   }
 
+  // since the data is inside the App component,
+  // the method which modifies the data is in the sam component
+  // in the fellow method, we create a function called removeContact 
+  // since the state is changed depends on the previous state
+  // we pass the current state as in this.setState
+  // contacts array will filter out whatever id we click and create a new array to update the states
+  // and then we pass the method as props to ListContacts, which the buttons are located
+  removeContact = (contact) => {
+    this.setState((currentState) => ({
+        contacts: currentState.contacts.filter((c) => {
+          return c.id !== contact.id
+        })
+    }))
+  }
+
   render() {
     return (
       <div>
         {/* the "contacts" inside {} represent the contacts list array above */}
-        <ListContacts contacts={this.state.contacts}/>
+        <ListContacts 
+          contacts={this.state.contacts}
+          onDeleteContact={this.removeContact}
+        />
       </div>
     );
   }
