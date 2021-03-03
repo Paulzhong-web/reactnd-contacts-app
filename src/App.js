3fa3b4ea6f81ba49b727036f337ec7a0b634f1e2
Paulzhong-web <paulzhong114@gmail.com>
2021-03-03 15:38:02 -0500

    Use componentDidMount to fetch contacts from the server

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
+// * means everything
+import * as ContactsAPI from './utils/ContactsAPI';
 
 class App extends Component {
+
   state = {
-    contacts: [
-      {
-        "id": "karen",
-        "name": "Karen Isgrigg",
-        "handle": "karen_isgrigg",
-        "avatarURL": "http://localhost:5001/karen.jpg"
-      },
-      {
-        "id": "richard",
-        "name": "Richard Kalehoff",
-        "handle": "richardkalehoff",
-        "avatarURL": "http://localhost:5001/richard.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "handle": "tylermcginnis",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-     ]
+    contacts: []
   }
+  
+  // lifecyle method to make API requests
+  componentDidMount() {
+    ContactsAPI.getAll()
+      .then((contacts) => {
+        this.setState(() => ({
+          contacts
+        }));
+      });
+  };
 
   // since the data is inside the App component,
   // the method which modifies the data is in the sam component
